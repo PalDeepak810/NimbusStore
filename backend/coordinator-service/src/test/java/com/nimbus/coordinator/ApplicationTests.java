3331fa0880cfe9c453db68c0aba6890d4f9e1967
Deepak <dp220001@gmail.com>
2026-06-27 23:00:01 +0530

    Initial backend services set-up

---
+++ backend/coordinator-service/src/test/java/com/nimbus/coordinator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbus.coordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
